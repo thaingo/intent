3118018b42863caf56aa40641227f19779d33e3d
thaingo <baothaingo@gmail.com>
2018-07-28 17:13:24 +0700

    set up list of Crimes for CrimeLab

--- app/src/main/java/com/example/thaingo/criminalintent/CrimeLab.java
+++ app/src/main/java/com/example/thaingo/criminalintent/CrimeLab.java
@@ -2,10 +2,17 @@ package com.example.thaingo.criminalintent;
 
 import android.content.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
 
+    private List<Crime> mCrimes;
+
     private CrimeLab(Context context) {
+        mCrimes = new ArrayList<>();
     }
 
     public static CrimeLab get(Context context) {
@@ -14,5 +21,19 @@ public class CrimeLab {
         }
 
         return sCrimeLab;
+    }
+
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime crime : mCrimes) {
+            if (crime.getId().equals(id)) {
+                return crime;
+            }
+        }
+
+        return null;
     }
 }
