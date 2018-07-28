715a301c605f4bdfb4e85f5f272e00a2bb038de0
thaingo <baothaingo@gmail.com>
2018-07-28 17:36:03 +0700

    fix typos

--- app/src/main/java/com/example/thaingo/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/thaingo/criminalintent/CrimeListFragment.java
@@ -2,9 +2,5 @@ package com.example.thaingo.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeListFragment extends SingleFragmentActivity {
-    @Override
-    protected Fragment createFragment() {
-        return new CrimeFragment();
-    }
+public class CrimeListFragment extends Fragment {
 }
