6e6002ac7628c53bd5e06813591d79956e6e0ba1
thaingo <baothaingo@gmail.com>
2018-07-28 17:16:03 +0700

    generated Crimes

--- app/src/main/java/com/example/thaingo/criminalintent/CrimeLab.java
+++ app/src/main/java/com/example/thaingo/criminalintent/CrimeLab.java
@@ -13,6 +13,12 @@ public class CrimeLab {
 
     private CrimeLab(Context context) {
         mCrimes = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime # " + i);
+            crime.setSolved(i % 2 == 0);
+            mCrimes.add(crime);
+        }
     }
 
     public static CrimeLab get(Context context) {
