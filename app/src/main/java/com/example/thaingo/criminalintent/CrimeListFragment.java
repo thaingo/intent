e21c0a8b24ebf2ce44a6538a9df096753d51b6c0
thaingo <baothaingo@gmail.com>
2018-07-28 23:16:18 +0700

    pull out all widgets needed for binding

--- app/src/main/java/com/example/thaingo/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/thaingo/criminalintent/CrimeListFragment.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -37,8 +38,14 @@ public class CrimeListFragment extends Fragment {
     }
 
     private class CrimeHolder extends RecyclerView.ViewHolder {
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+
         public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.list_item_crime, parent, false));
+
+            mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
+            mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
         }
     }
 
