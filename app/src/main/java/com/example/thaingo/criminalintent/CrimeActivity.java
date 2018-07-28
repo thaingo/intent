d67b5f92df5971319d8a0418ca6a077dbe3e9e5e
thaingo <baothaingo@gmail.com>
2018-07-28 17:28:57 +0700

    refactor CrimeActivity

--- app/src/main/java/com/example/thaingo/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/example/thaingo/criminalintent/CrimeActivity.java
@@ -1,23 +1,11 @@
 package com.example.thaingo.criminalintent;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class CrimeActivity extends AppCompatActivity {
+public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-        if (fragment == null) {
-            fragment = new CrimeFragment();
-
-            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
-        }
+    protected Fragment createFragment() {
+        return new CrimeFragment();
     }
 }
