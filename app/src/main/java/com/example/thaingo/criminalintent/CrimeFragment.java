b6d23185bb2dac663ebf878a4e728648ebdf560f
thaingo <baothaingo@gmail.com>
2018-07-28 16:24:46 +0700

    implemented fragment lifecycle

---
+++ app/src/main/java/com/example/thaingo/criminalintent/CrimeFragment.java
@@ -0,0 +1,28 @@
+package com.example.thaingo.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime, container, false);
+        return view;
+    }
+
+}
