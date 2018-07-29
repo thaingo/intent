f2d124397893885e4381e6634124e668f1c96691
thaingo <baothaingo@gmail.com>
2018-07-29 17:17:17 +0700

    used newInstance of CrimeFragment in CrimeActivity

--- app/src/main/java/com/example/thaingo/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/example/thaingo/criminalintent/CrimeActivity.java
@@ -7,11 +7,13 @@ import android.support.v4.app.Fragment;
 import java.util.UUID;
 
 public class CrimeActivity extends SingleFragmentActivity {
-    public static final String EXTRA_CRIME_ID = "com.example.thaingo.criminalintent.crime_id";
+    private static final String EXTRA_CRIME_ID = "com.example.thaingo.criminalintent.crime_id";
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+
+        return CrimeFragment.newInstance(crimeId);
     }
 
     public static Intent newIntent(Context packageContext, UUID crimeId) {
