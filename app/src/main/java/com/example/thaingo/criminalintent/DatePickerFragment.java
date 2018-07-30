b966429c23710e2fce063ea0aef0ac4330458177
thaingo <baothaingo@gmail.com>
2018-07-30 23:27:51 +0700

    extracted date and initialized DatePicker

--- app/src/main/java/com/example/thaingo/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/example/thaingo/criminalintent/DatePickerFragment.java
@@ -1,6 +1,7 @@
 package com.example.thaingo.criminalintent;
 
 import android.app.Dialog;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
@@ -25,10 +26,21 @@ public class DatePickerFragment extends DialogFragment {
 
         return dpf;
     }
+
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Date date = (Date) getArguments().getSerializable(ARG_DATE);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
         View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
+
+        mDatePicker = (DatePicker) view.findViewById(R.id.dialog_date_picker);
+        mDatePicker.init(year, month, day, null);
 
         return new AlertDialog.Builder(getActivity())
                 .setView(view)
