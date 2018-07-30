ecf0e2c6543dd095b5365bc3e7e5d430713f0520
thaingo <baothaingo@gmail.com>
2018-07-30 17:34:19 +0700

    decommissed CrimeActivity

--- app/src/main/java/com/example/thaingo/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/example/thaingo/criminalintent/CrimePagerActivity.java
@@ -1,5 +1,7 @@
 package com.example.thaingo.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,8 +11,11 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import java.util.List;
+import java.util.UUID;
 
 public class CrimePagerActivity extends AppCompatActivity {
+    private static final String EXTRA_CRIME_ID = "com.example.thaingo.criminalintent.crime_id";
+
     private ViewPager mViewPager;
     private List<Crime> mCrimes;
 
@@ -18,6 +23,8 @@ public class CrimePagerActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);
+
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
 
         mViewPager = (ViewPager) findViewById(R.id.crime_view_pager);
         mCrimes = CrimeLab.get(this).getCrimes();
@@ -35,5 +42,12 @@ public class CrimePagerActivity extends AppCompatActivity {
                 return mCrimes.size();
             }
         });
+    }
+
+    public static Intent newIntent(Context packageContext, UUID crimeId) {
+        Intent intent = new Intent(packageContext, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+
+        return intent;
     }
 }
