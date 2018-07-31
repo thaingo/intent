50e8c675dbba62e70c70d4615950d2bc36e935fb
thaingo <baothaingo@gmail.com>
2018-07-31 23:09:12 +0700

    explicit tell FragmentManager that the fragment should receive a call to onCreateOptionsMenu when the hosting activity receives its onCreateOptionsMenu call back from OS

--- app/src/main/java/com/example/thaingo/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/thaingo/criminalintent/CrimeListFragment.java
@@ -22,6 +22,12 @@ public class CrimeListFragment extends Fragment {
     private RecyclerView mRecyclerView;
     private CrimeAdapter mAdapter;
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
