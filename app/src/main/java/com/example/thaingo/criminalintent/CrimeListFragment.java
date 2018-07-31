6ab326f809ac946f29d7cce91a22f548b00d26e1
thaingo <baothaingo@gmail.com>
2018-07-31 23:03:46 +0700

    created menu in CrimeListFragment

--- app/src/main/java/com/example/thaingo/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/thaingo/criminalintent/CrimeListFragment.java
@@ -9,6 +9,8 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -31,6 +33,12 @@ public class CrimeListFragment extends Fragment {
         updateUI();
 
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_crime_list, menu);
     }
 
     @Override
