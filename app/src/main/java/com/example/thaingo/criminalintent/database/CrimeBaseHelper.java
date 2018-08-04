296e42fdbde4bc14f2bc8c37d935c9b17205d0c1
thaingo <baothaingo@gmail.com>
2018-08-04 22:46:42 +0700

    create crime table

--- app/src/main/java/com/example/thaingo/criminalintent/database/CrimeBaseHelper.java
+++ app/src/main/java/com/example/thaingo/criminalintent/database/CrimeBaseHelper.java
@@ -3,6 +3,7 @@ package com.example.thaingo.criminalintent.database;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import com.example.thaingo.criminalintent.database.CrimeDbSchema.CrimeTable;
 
 public class CrimeBaseHelper extends SQLiteOpenHelper {
     private static final int VERSION = 1;
@@ -14,7 +15,14 @@ public class CrimeBaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("create table " + CrimeTable.NAME + "(" +
+                           "_id integer primary key autoincrement, " +
+                           CrimeTable.Cols.UUID + ", " +
+                           CrimeTable.Cols.TITLE + ", " +
+                           CrimeTable.Cols.DATE + ", " +
+                           CrimeTable.Cols.SOLVED +
+                           ")"
+        );
     }
 
     @Override
