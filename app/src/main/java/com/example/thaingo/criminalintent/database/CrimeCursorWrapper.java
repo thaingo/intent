045aff14e9b4fa6dfa0c215ab6bc38681781895c
thaingo <baothaingo@gmail.com>
2018-08-05 10:20:10 +0700

    added getCrime method

--- app/src/main/java/com/example/thaingo/criminalintent/database/CrimeCursorWrapper.java
+++ app/src/main/java/com/example/thaingo/criminalintent/database/CrimeCursorWrapper.java
@@ -2,6 +2,11 @@ package com.example.thaingo.criminalintent.database;
 
 import android.database.Cursor;
 import android.database.CursorWrapper;
+import com.example.thaingo.criminalintent.Crime;
+import com.example.thaingo.criminalintent.database.CrimeDbSchema.CrimeTable;
+
+import java.util.Date;
+import java.util.UUID;
 
 public class CrimeCursorWrapper extends CursorWrapper {
     /**
@@ -11,5 +16,19 @@ public class CrimeCursorWrapper extends CursorWrapper {
      */
     public CrimeCursorWrapper(Cursor cursor) {
         super(cursor);
+    }
+
+    public Crime getCrime() {
+        String uuid = getString(getColumnIndex(CrimeTable.Cols.UUID));
+        String title = getString(getColumnIndex(CrimeTable.Cols.TITLE));
+        long date = getLong(getColumnIndex(CrimeTable.Cols.DATE));
+        int isSolved = getInt(getColumnIndex(CrimeTable.Cols.SOLVED));
+
+        Crime crime = new Crime(UUID.fromString(uuid));
+        crime.setTitle(title);
+        crime.setDate(new Date(date));
+        crime.setSolved(isSolved != 0);
+
+        return crime;
     }
 }
