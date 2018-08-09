6e017ec848912265a284f1d1f70720ab55918848
thaingo <baothaingo@gmail.com>
2018-08-09 15:46:17 +0700

    scale bitmap object

---
+++ app/src/main/java/com/example/thaingo/criminalintent/PictureUtils.java
@@ -0,0 +1,30 @@
+package com.example.thaingo.criminalintent;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+public class PictureUtils {
+    public static Bitmap getScaleBitmap(String path, int destWidth, int destHeight) {
+        // read in the dimensions of the image on disk
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(path, options);
+
+        float srcWidth = options.outWidth;
+        float srcHeight = options.outHeight;
+
+        // figure out how much to scale down by
+        int inSampleSize = 1;
+        if (srcHeight > destHeight || srcWidth > destWidth) {
+            float heightScale = srcHeight / destHeight;
+            float widthScale = srcWidth / destWidth;
+            inSampleSize = Math.round(heightScale > widthScale ? heightScale : widthScale);
+        }
+
+        options = new BitmapFactory.Options();
+        options.inSampleSize = inSampleSize;
+
+        // read in and create final bitmap
+        return BitmapFactory.decodeFile(path, options);
+    }
+}
